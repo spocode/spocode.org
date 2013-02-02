7eccf08505c9cd6d895bb3df198975f9b556b018
Dan Gayle <dangayle@gmail.com>
2013-02-01 16:00:49 -0800

    Initial setup

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='spocode',
       version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
-      url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
+      description='Website for SpoCode Hackathon',
+      author='Dan Gayle',
+      author_email='dangayle@gmail.com',
+      url='http://www.spocode.org',
+      install_requires=['Bottle', 'Pymongo'],
      )
---
+++ wsgi/index.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import cgi
+import bottle
+import pymongo
+
+bottle.debug(True)
+
+mongo_con = pymongo.Connection(os.environ['OPENSHIFT_MONGODB_DB_HOST'],
+                            int(os.environ['OPENSHIFT_MONGODB_DB_PORT']))
+
+mongo_db = mongo_con[os.environ['OPENSHIFT_APP_NAME']]
+mongo_db.authenticate(os.environ['OPENSHIFT_MONGODB_DB_USERNAME'],
+                            os.environ['OPENSHIFT_MONGODB_DB_PASSWORD'])
+bottle.TEMPLATE_PATH.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR'],
+                                                'wsgi', 'views'))
+
+
+@bottle.get('/')
+def index():
+    '''
+    Create home page.
+    TODO: Add more info
+    '''
+
+    return "hi"
+
+
+application = bottle.default_app()
